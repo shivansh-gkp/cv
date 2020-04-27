d06aa776f5073b9f56c466c814d5cd5e4c8c6f5b
shivansh-gkp <homepandey@gmail.com>
2020-04-27 20:53:28 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-$(".cloud-1").animate({right: '20%', opacity: '0.9'}, 1300);
-$(".cloud-2").animate({right:'73%', opacity:'0.9'},1200);
-$(".mountain").animate({opacity:'1'}, 2000);
+setTimeout(function(){
+  $(".cloud-1").animate({right: '20%', opacity: '0.9'}, 1300);
+  $(".cloud-2").animate({right:'73%', opacity:'0.9'},1200);
+  $(".mountain").animate({opacity:'1'}, 2000);
+
+}, 900);
